336b64b9fbfdd023a314e1c0cc9f7f3d6a81326b
Arthur Gibert <arthurgibert@free.fr>
2024-01-18 11:24:34 +0100

    Ajout de la classe Student

---
+++ src/fr/fms/classe/Student.java
@@ -0,0 +1,29 @@
+package fr.fms.classe;
+
+public class Student extends Person {
+	
+	// Propriétés de la classe :
+	private int id;
+	private static int numberInstance = 0;
+	
+	{
+		numberInstance ++;
+	}
+	
+	// Constructeurs :
+	public Student(String name, String lastName, int age, Adress adress) {
+		super(name, lastName, age, adress);
+		this.id = numberInstance;
+	}
+	
+	// Méthodes :
+	@Override
+	public String toString() {
+		return "Elève: " + getName() + " " + getLastName() + " - Adress: " + getAdress() + " - Id: " + id;
+	}
+	
+	// Accésseurs :
+	public int getId() {
+		return id;
+	}
+}
